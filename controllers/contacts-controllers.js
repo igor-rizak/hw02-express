a63d72efde1936f11f02aa2d54602ec25009054b
igor-rizak <igor.rizak@ukr.net>
2024-01-22 20:55:28 +0200

    last commit

--- controllers/contacts-controllers.js
+++ controllers/contacts-controllers.js
@@ -1,4 +1,4 @@
-import Contacts from "../models/contacts.js";
+import Contact from "../models/contacts.js";
 
 import { HttpError } from "../helpers/index.js";
 
@@ -8,7 +8,7 @@ const getAll = async (req, res) => {
     const {_id: owner} = req.user;
     const {page = 1, limit = 10} = req.query;
     const skip = (page - 1) * limit;
-    const result = await Contacts.find({owner}, "-createdAt -updatedAt", {skip, limit}).populate("owner", "username");
+    const result = await Contact.find({owner}, "-createdAt -updatedAt", {skip, limit}).populate("owner", "username");
 
     res.json(result);
 }
@@ -18,7 +18,7 @@ const getById = async (req, res) => {
     const {_id: owner} = req.user;
     const result = await User.findOne({_id, owner});
     if (!result) {
-        throw HttpError(404, `Movie with id=${id} not found`);
+        throw HttpError(404, `id=${id} is not defined`);
     }
 
     res.json(result);
@@ -26,7 +26,7 @@ const getById = async (req, res) => {
 
 const add = async (req, res) => {
     const {_id: owner} = req.user;
-    const result = await Contacts.create({...req.body, owner});
+    const result = await Contact.create({...req.body, owner});
 
     res.status(201).json(result)
 }
@@ -34,9 +34,9 @@ const add = async (req, res) => {
 const updateById = async (req, res) => {
     const { id: _id } = req.params;
     const {_id: owner} = req.user;
-    const result = await Contacts.findOneAndUpdate({_id, owner}, req.body);
+    const result = await Contact.findOneAndUpdate({_id, owner}, req.body);
     if (!result) {
-        throw HttpError(404, `Movie with id=${id} not found`);
+        throw HttpError(404, `id=${id} is not defined`);
     }
 
     res.json(result);
@@ -45,9 +45,9 @@ const updateById = async (req, res) => {
 const deleteById = async (req, res) => {
     const { id: _id } = req.params;
     const {_id: owner} = req.user;
-    const result = await Contacts.findOneAndDelete({_id, owner});
+    const result = await Contact.findOneAndDelete({_id, owner});
     if (!result) {
-        throw HttpError(404, `Movie with id=${id} not found`);
+        throw HttpError(404, `id=${id} is not defined`);
     }
 
     res.json({
